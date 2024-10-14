7b9a39021e21df64b50a38d57ed276b157d006d0
Albert Recuero Campasol <albertrc7573@gmail.com>
2024-10-14 18:07:49 +0200

    Modifico pequeñas partes del proyecto!

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const button = document.getElementById('btnprimario');
 const contador= document.getElementById('contador1');
 const desbloqueo = document.getElementById('chckdes');
+const desbloqueo2 = document.getElementById('chckdes2');
+const imagendes = document.getElementById('imagen2');
 let suma = 0;
 
 
@@ -14,10 +15,17 @@ function funcion(){
    contador.innerText= suma;
    }
 
-   if(contador.innerText >= 50){
+   if(contador.innerText >= 50 && contador.innerText <= 190){
     suma += 10;
     desbloqueo.innerText = "Has desbloqueado:"
     contador.innerText= suma;
+    imagendes.src = '/img/desbloqueo.jpg'
+   }
+
+   if (contador.innerText >= 200){
+      suma += 100
+      desbloqueo2.innerText = "Has desbloqueado:"
+      contador.innerText = suma;
    }
    
 }
